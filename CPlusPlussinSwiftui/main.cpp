4d387301b81e0cba0be58bd1c36207ee1d7a6f7a
KamsQue <kamqasim1@gmail.com>
2023-09-04 13:24:04 +0200

    commit initial

---
+++ CPlusPlussinSwiftui/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  CPlusPlussinSwiftui
+//
+//  Created by KamsQue on 04/09/2023.
+//
+
+#include "main.h"
+
+
+const char* test(){
+  return "C++ Code is Called";
+}
---
+++ CPlusPlussinSwiftui/main.h
@@ -0,0 +1,23 @@
+//
+//  main.hpp
+//  CPlusPlussinSwiftui
+//
+//  Created by KamsQue on 04/09/2023.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const char* test();
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
