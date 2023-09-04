1a9aca2c34866935bf07229fb480798f8f463102
KamsQue <kamqasim1@gmail.com>
2023-09-04 12:41:25 +0200

    Initial Commit

---
+++ CPlusPlussinSwiftui/CPlusPlussinSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPlusPlussinSwiftuiApp.swift
+//  CPlusPlussinSwiftui
+//
+//  Created by KamsQue on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CPlusPlussinSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPlusPlussinSwiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPlusPlussinSwiftui
+//
+//  Created by KamsQue on 04/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
